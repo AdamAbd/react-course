ae0dee949acba2925b5e5e284161bfa1d5ec5d8a
Adam Abdurrahman <adamabd14113@gmail.com>
2021-09-13 09:49:23 +0700

    new app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import Todo from './components/Todo';
-
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text='Learn React'/>
-      <Todo text='Master React'/>
-      <Todo text='Explore the full React Course'/>
     </div>
   );
 }
--- src/components/Backdrop.js
+++
@@ -1,7 +0,0 @@
-function Backdrop(props) {
-    return (
-        <div className='backdrop' onClick={props.onCancel} />
-    );
-}
-
-export default Backdrop;
